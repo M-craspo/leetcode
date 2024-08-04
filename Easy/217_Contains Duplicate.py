b128f2353e46dcaa498ef0e1ea7ff632d3e16973
M-craspo <elbahymahmoud206@gmail.com>
2024-08-04 21:50:37 +0300

    '#'

--- Easy/217_Contains Duplicate.py
+++ Easy/217_Contains Duplicate.py
@@ -1,7 +1,4 @@
 class Solution(object):
     def containsDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
+        return(len(set(nums)) != len(nums))
         

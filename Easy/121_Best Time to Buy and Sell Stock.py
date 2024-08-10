349d27dd95393b0acd4483efc54a6c047b2ec706
M-craspo <elbahymahmoud206@gmail.com>
2024-08-10 04:37:05 +0300

    first commit

---
+++ Easy/121_Best Time to Buy and Sell Stock.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        x= min(prices)
+        sliced_list = prices[prices.index(x):]
+        max_number = max(sliced_list)
+        return max_number - x
+    # class Solution(object):
+    # def maxProfit(self, prices):
+    #     if not prices:
+    #         return 0
+        
+    #     min_price = float('inf')
+    #     max_profit = 0
+        
+    #     for price in prices:
+    #         if price < min_price:
+    #             min_price = price
+    #         else:
+    #             current_profit = price - min_price
+    #             max_profit = max(max_profit, current_profit)
+        
+    #     return max_profit
+s = Solution()
+print(s.maxProfit([2,4,1]))

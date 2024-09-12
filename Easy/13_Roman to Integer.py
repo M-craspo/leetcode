1b6cfe684a69c432aab4439e6d1f6525165302cf
M-craspo <elbahymahmoud206@gmail.com>
2024-09-13 02:53:49 +0300

    first commit

---
+++ Easy/13_Roman to Integer.py
@@ -0,0 +1,32 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_values = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        total = 0
+        prev_value = 0
+        
+        for char in s[::-1]:
+            current_value = roman_values[char]
+            
+            if current_value < prev_value:
+                total -= current_value
+            else:
+                total += current_value
+            
+            prev_value = current_value
+        
+        return total
+
+# Test the solution
+solution = Solution()
+print(solution.romanToInt("III"))  # Should output 3
+print(solution.romanToInt("LVIII"))  # Should output 58
+print(solution.romanToInt("MCMXCIV"))  # Should output 1994
